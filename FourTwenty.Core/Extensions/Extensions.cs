f44b5d90cd060550c29c54684c9aca4b9f2b4444
Anton Yaroshenko <kpakadijl@hotmail.com>
2019-06-30 14:45:42 +0300

    added temp folder fix extension

---
+++ FourTwenty.Core/Extensions/Extensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+namespace FourTwenty.Core.Extensions
+{
+    public static class Extensions
+    {
+        /// <summary>
+        /// fix for default temp folder limit (https://docs.microsoft.com/ru-ru/dotnet/api/system.io.path.gettempfilename?view=netcore-2.2)
+        /// </summary>
+        /// <param name="env">Current website environment</param>
+        /// <param name="folderName">Newly created temporary folder name</param>
+        public static void FixTemporaryFolder(this IHostingEnvironment env, string folderName = "TEMP")
+        {
+            #region 
+            var tempPath = Path.Combine(env.WebRootPath, folderName);
+            if (!Directory.Exists(tempPath))
+                Directory.CreateDirectory(tempPath);
+
+            Environment.SetEnvironmentVariable("TEMP", tempPath);
+            Environment.SetEnvironmentVariable("TMP", tempPath);
+            #endregion
+
+        }
+    }
+}
