0d3972c590df0d337256e42ea500f118bdd2060e
Anton Yaroshenko <kpakadijl@hotmail.com>
2019-07-13 13:06:01 +0300

    Model exception added

---
+++ FourTwenty.Core/Exceptions/ModelException.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FourTwenty.Core.Exceptions
+{
+    public class ModelException : Exception
+    {
+        public enum ModelExceptionCode
+        {
+            NotFound,
+            Conflict,
+            Custom
+        }
+
+        public ModelExceptionCode ErrorCode { get; set; }
+
+        public ModelException(ModelExceptionCode code, string message) : base(message)
+        {
+            ErrorCode = code;
+        }
+
+        public ModelException(ModelExceptionCode code, string message, Exception inner) : base(message, inner)
+        {
+            ErrorCode = code;
+        }
+
+        public ModelException(ModelExceptionCode code)
+        {
+            ErrorCode = code;
+        }
+    }
+}
