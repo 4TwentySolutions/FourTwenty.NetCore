3fdeed11fd88c74078b5c4fe7985bb1a7852f835
Anton Yaroshenko <kpakadijl@hotmail.com>
2020-12-07 16:18:40 +0200

    Email sender refactoring

---
+++ FourTwenty.Core/Options/EmailSenderOptions.cs
@@ -0,0 +1,31 @@
+﻿namespace FourTwenty.Core.Options
+{
+    public class EmailSenderOptions
+    {
+        public EmailSenderOptions(string host, int port, bool enableSsl, string userName, string password, bool useCertificateValidation, string fromName)
+        {
+            Host = host;
+            Port = port;
+            EnableSsl = enableSsl;
+            UserName = userName;
+            Password = password;
+            UseCertificateValidation = useCertificateValidation;
+            FromName = fromName;
+        }
+
+        public EmailSenderOptions()
+        {
+            
+        }
+
+        public string Host { get; set; }
+
+        public int Port { get; set; }
+        public bool EnableSsl { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public bool UseCertificateValidation { get; set; }
+        public string FromName { get; set; }
+
+    }
+}
--- FourTwenty.Core/Services/EmailSender.cs
+++ FourTwenty.Core/Services/EmailSender.cs
@@ -1,29 +1,32 @@
 ﻿using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using FourTwenty.Core.Interfaces;
+using FourTwenty.Core.Options;
 using MailKit.Net.Smtp;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Options;
 using MimeKit;
 
 namespace FourTwenty.Core.Services
 {
-    public class EmailSender : IEmailService
+    public class EmailSender : IEmailService, IEmailSender
     {
-        // Our private configuration variables
-        private readonly string _host;
-        private readonly int _port;
-        private readonly bool _enableSsl;
-        private readonly string _userName;
-        private readonly string _password;
 
-        public EmailSender(string host, int port, bool enableSsl, string userName, string password)
+        private readonly EmailSenderOptions _emailSenderOptions;
+
+        public EmailSender(string host, int port, bool enableSsl, string userName, string password, bool useCertificateValidation = false, string fromName = "My Application") =>
+            _emailSenderOptions = new EmailSenderOptions(host, port, enableSsl, userName, password,
+                useCertificateValidation, fromName);
+
+
+        public EmailSender(IOptions<EmailSenderOptions> emailOptions)
         {
-            this._host = host;
-            this._port = port;
-            this._enableSsl = enableSsl;
-            this._userName = userName;
-            this._password = password;
+            _emailSenderOptions = emailOptions.Value;
         }
 
 
@@ -32,49 +35,80 @@ namespace FourTwenty.Core.Services
             return SendEmailAsync(email, subject, htmlMessage, null);
         }
 
-        public async Task SendEmailAsync(string email, string subject, string htmlMessage, List<IFormFile> attachments)
+        public virtual async Task SendEmailAsync(string email, string subject, string htmlMessage, List<IFormFile> attachments)
         {
             var message = new MimeMessage();
-            message.From.Add(new MailboxAddress("MACS Application", _userName));
+            message.From.Add(new MailboxAddress(_emailSenderOptions.FromName, _emailSenderOptions.UserName));
             message.To.Add(new MailboxAddress(email, email));
             message.Subject = subject;
 
-            var body = new TextPart("plain")
+            var body = new TextPart("html")
             {
-                Text = htmlMessage,
+                Text = htmlMessage
             };
-            
-            var multipart = new Multipart("mixed");
-            foreach (var file in attachments)
+
+            var multipart = new Multipart("mixed") { body };
+
+            foreach (var attachment in attachments.Select(file => new MimePart(file.ContentType)
             {
-                // create an image attachment for the file located at path
-                var attachment = new MimePart("text/plain")
-                {
-                    Content = new MimeContent(file.OpenReadStream()),
-                    ContentDisposition = new ContentDisposition(ContentDisposition.Attachment),
-                    ContentTransferEncoding = ContentEncoding.Base64,
-                    FileName = Path.GetFileName(file.FileName)
-                };
-                // now create the multipart/mixed container to hold the message text and the
-                // image attachment
-                multipart.Add(body);
+                Content = new MimeContent(file.OpenReadStream()),
+                ContentDisposition = new ContentDisposition(ContentDisposition.Attachment),
+                ContentTransferEncoding = ContentEncoding.Base64,
+                FileName = Path.GetFileName(file.FileName)
+            }))
+            {
                 multipart.Add(attachment);
             }
-           
+
             message.Body = multipart;
-            using (var client = new SmtpClient())
+            using var client = new SmtpClient();
+            if (_emailSenderOptions.UseCertificateValidation)
+                client.ServerCertificateValidationCallback = ValidateCertificate;
+
+            await client.ConnectAsync(_emailSenderOptions.Host, _emailSenderOptions.Port, _emailSenderOptions.EnableSsl);
+
+            await client.AuthenticateAsync(_emailSenderOptions.UserName, _emailSenderOptions.Password);
+
+            await client.SendAsync(message);
+            await client.DisconnectAsync(true);
+        }
+
+        protected virtual bool ValidateCertificate(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+        {
+            const SslPolicyErrors ignoredErrors =
+                SslPolicyErrors.RemoteCertificateChainErrors |  // self-signed
+                SslPolicyErrors.RemoteCertificateNameMismatch;  // name mismatch
+
+            // If the certificate is a valid, signed certificate, return true.
+            if ((sslPolicyErrors & ~ignoredErrors) == SslPolicyErrors.None)
+                return true;
+
+            // If there are errors in the certificate chain, look at each error to determine the cause.
+            if ((sslPolicyErrors & SslPolicyErrors.RemoteCertificateChainErrors) == 0) return false;
+
+            // In all other cases, return false.
+            if (chain?.ChainStatus == null) return true;
+            foreach (X509ChainStatus status in chain.ChainStatus)
             {
-                // For demo-purposes, accept all SSL certificates (in case the server supports STARTTLS)
-                //client.ServerCertificateValidationCallback = (s, c, h, e) => true;
+                if (certificate.Subject == certificate.Issuer &&
+                    (status.Status == X509ChainStatusFlags.UntrustedRoot))
+                {
+                    // Self-signed certificates with an untrusted root are valid. 
+                    continue;
+                }
 
-                await client.ConnectAsync(_host, _port, _enableSsl);
-
-                // Note: only needed if the SMTP server requires authentication
-                client.Authenticate(_userName, _password);
-
-                await client.SendAsync(message);
-                await client.DisconnectAsync(true);
+                if (status.Status != X509ChainStatusFlags.NoError)
+                {
+                    // If there are any other errors in the certificate chain, the certificate is invalid,
+                    // so the method returns false.
+                    return false;
+                }
             }
+
+            // When processing reaches this line, the only errors in the certificate chain are 
+            // untrusted root errors for self-signed certificates. These certificates are valid
+            // for default Exchange server installations, so return true.
+            return true;
         }
     }
 }
